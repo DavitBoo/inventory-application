85200f2fa5a14944f01dbfcba6109fde2e0d2f81
DavitBoo <davidmarras@gmail.com>
2023-08-16 16:04:38 +0200

    schemas created

---
+++ models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+CategorySchema.virtual("url").get(function () {
+  return `/categories/${this._id}`;
+});
+
+// export the model
+module.exports = mongoose.model("Category", CategorySchema);
---
+++ models/item.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  category: [{ type: Schema.Types.ObjectId, ref: "Category" }],
+});
+
+ItemSchema.virtual("url").get(function () {
+  return `/items/${this._id}`;
+});
+
+// export the model
+module.exports = mongoose.model("Item", ItemSchema);
