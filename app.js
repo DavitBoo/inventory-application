97f55a35989995a47d008a4520920a29c4244a3f
DavitBoo <davidmarras@gmail.com>
2023-09-10 00:15:25 +0200

    ready for deploy

--- app.js
+++ app.js
@@ -10,7 +10,28 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const inventoryRouter = require("./routes/inventory");
 
+const compression = require("compression");
+const helmet = require("helmet");
+
 var app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "cdn.jsdelivr.net"],
+    },
+  })
+);
 
 //set up mongoose
 const mongoose = require("mongoose");
@@ -31,6 +52,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
